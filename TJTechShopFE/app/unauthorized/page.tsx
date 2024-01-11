7832954e71ea294a004423f78f4fdec379144687
TJ1208 <TaylorJ1208@yahoo.com>
2024-01-11 17:22:15 -0500

    Replaces ' -> &apos;

--- TJTechShopFE/app/unauthorized/page.tsx
+++ TJTechShopFE/app/unauthorized/page.tsx
@@ -8,14 +8,14 @@ const Unauthorized = () => {
     
     useEffect(() => {
         setTimeout(() => {
-            router.push("/")
+            router.push("/techshop")
         }, 3000)
     }, [])
     return (
         <>
             <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute text-center">
-                <h1 className="opacity-60 text-xl p-2">Oops! Looks like you don't belong here...</h1>
-                <p className="opacity-60 text-sm italic p-2 border-b">Redirecting In...<span className="font-semibold text-white"></span></p>
+                <h1 className="opacity-60 text-xl p-2">Oops! Looks like you don&apos;t belong here...</h1>
+                <p className="opacity-60 text-sm italic p-2 border-b">Redirecting...<span className="font-semibold text-white"></span></p>
             </div>
 
         </>
