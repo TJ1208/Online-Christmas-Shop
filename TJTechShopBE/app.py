740e6359247239dd8ecc5db48a96a5b65331a28e
TJ1208 <TaylorJ1208@yahoo.com>
2024-01-13 00:35:10 -0500

    Adds Changes JWT_COOKIE_DOMAIN -> techspec.azurewebsites.net

--- TJTechShopBE/app.py
+++ TJTechShopBE/app.py
@@ -41,7 +41,7 @@ app.config["JWT_COOKIE_SECURE"] = True
 app.config["JWT_COOKIE_SAMESITE"] = "None"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
-app.config["JWT_COOKIE_DOMAIN"] = ".azurewebsites.net"
+app.config["JWT_COOKIE_DOMAIN"] = "techspec.azurewebsites.net"
 app.config["JWT_COOKIE_CSRF_PROTECT"] = True
 api = Api(app)
 
