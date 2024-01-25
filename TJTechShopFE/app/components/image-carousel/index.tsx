dd8a9d14e90bbb4892321677c220c208c4c7f3d3
TJ1208 <TaylorJ1208@yahoo.com>
2024-01-25 00:04:24 -0500

    Changes ImageCarousel to SSR

--- TJTechShopFE/app/components/image-carousel/index.tsx
+++ TJTechShopFE/app/components/image-carousel/index.tsx
@@ -1,21 +1,9 @@
-"use client"
-
 import { ImageModel } from "@/app/models/image"
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useState } from "react"
 
 const ImageCarousel = (images: { images: ImageModel[] }) => {
-    const [carouselCounter, setCarouselCounter] = useState<number>(0)
 
     return (
         <>
-            <style jsx global>{`
-          .Sirv 
-            width: 500px;
-            height: 500px;
-          `}
-            </style>
             <div className="Sirv lg:w-1 -z-30" data-options="fullscreen.enable:false;">
                 {
                     images.images.map(image => (
