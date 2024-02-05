10b5a081b9c73f81cae735e2ff1ff60180d11324
TJ1208 <TaylorJ1208@yahoo.com>
2024-02-05 12:31:13 -0500

    Updates Order Table

--- TJTechShopBE/models/order.py
+++ TJTechShopBE/models/order.py
@@ -5,10 +5,10 @@ class OrderModel(db.Model):
     __tablename__ = "order"
 
     order_id = db.Column(db.BigInteger, primary_key=True)
-    user_id = db.Column(db.BigInteger, db.ForeignKey("user.user_id", ondelete="CASCADE"), unique=False, nullable=False)
-    address_id = db.Column(db.BigInteger, db.ForeignKey("address.address_id", ondelete="CASCADE"), unique=False, nullable=False)
+    user_id = db.Column(db.BigInteger, db.ForeignKey("user.user_id"), unique=False, nullable=False)
+    address_id = db.Column(db.BigInteger, db.ForeignKey("address.address_id"), unique=False, nullable=False)
     total = db.Column(db.Integer, nullable=False)
     create_time = db.Column(db.Date, nullable=False)
-    user = db.relationship("UserModel", backref="user", cascade="all, delete", lazy=True)
-    address = db.relationship("AddressModel", backref="AddressModel", cascade="all, delete", lazy=True)
+    user = db.relationship("UserModel", backref="user", lazy=True)
+    address = db.relationship("AddressModel", backref="AddressModel", lazy=True)
     products = db.relationship("ProductModel", secondary="order_product", back_populates="orders")
