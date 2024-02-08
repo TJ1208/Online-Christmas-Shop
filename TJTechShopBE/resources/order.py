b8debae43800b28a870c55bd6f1fc3216765a1d2
TJ1208 <TaylorJ1208@yahoo.com>
2024-02-08 13:41:17 -0500

    Updates Mailtrap Mail Integration

--- TJTechShopBE/resources/order.py
+++ TJTechShopBE/resources/order.py
@@ -1,11 +1,11 @@
-
 from flask.views import MethodView
+from flask_mail import Message
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
+
 from models.order import OrderModel
-from resources.db import db
+from resources.db import db, mail
 from schemas import PlainOrderSchema, UpdateOrderSchema, OrderSchema
-import mailtrap as mt
 
 blp = Blueprint("OrderSchema", __name__, description="Operations on order")
 
@@ -38,7 +38,6 @@ class OrderExt(MethodView):
     def get(self, order_id):
         order = OrderModel.query.get_or_404(order_id,
                                             description=f"No order exists with the id: {order_id}")
-        send_order_details(order)
         return order
 
     @blp.arguments(UpdateOrderSchema)
@@ -61,20 +60,28 @@ class OrderExt(MethodView):
         db.session.commit()
         return f"Order with the id, {order_id}, has been removed."
 
-
-def send_order_details(order):
-    print(order.user.email)
-    try:
-        mail = mt.Mail(
-            sender=mt.Address(email="TaylorJ1208@yahoo.com"),
-            to=[mt.Address(email=order.user.email)],
-            subject="TJTechShop Order#12345",
-            text="Thank you for your purchase!",
-            category="Integration Test"
-        )
-        client = mt.MailtrapClient(token="4909bbe4e80053f7457f598819de759d")
-        client.send(mail)
-    except Exception as e:
-        print(e)
-    # for product in order.products:
-    #     print(product)
+    @blp.response(200)
+    def post(self, order_id):
+        order = OrderModel.query.get_or_404(order_id,
+                                            description=f"No order exists with the id: {order_id}")
+        print(order)
+        try:
+            msg = Message(
+                subject="TJTechShop Order#12345",
+                sender="mailtrap@tcjcoding.com",
+                recipients=[order.user.email],
+                body="Thank you for your purchase!")
+            mail.send(msg)
+            # mail = mt.Mail(
+            #     sender=mt.Address(email="TaylorJ1208@yahoo.com"),
+            #     to=[mt.Address(email=order.user.email)],
+            #     subject="TJTechShop Order#12345",
+            #     text="Thank you for your purchase!",
+            #     category="Integration Test"
+            # )
+            # client = mt.MailtrapClient(token="4909bbe4e80053f7457f598819de759d")
+            # client.send(mail)
+        except Exception as e:
+            print(e)
+        # for product in order.products:
+        #     print(product)
